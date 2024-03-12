1cad2d5101ab0728002cb341a855ac0b6331f4a6
Alba-NR <alba.navarro.ros@gmail.com>
2024-03-12 00:00:11 +0000

    try to fix google fonts

--- _nuxt/static/1710200781/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002F","\u002Fprojects","\u002Ffun","\u002Fabout","\u002Fprojects\u002Fopengl-java","\u002Fprojects\u002Faugmented-room","\u002Fprojects\u002Fnereus","\u002Fprojects\u002Ftriolingo"]})
